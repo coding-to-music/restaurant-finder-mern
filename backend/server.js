4570700bd10386319ca602167555b4e57a005100
Bartek Wilczyński <bartekw2213@gmail.com>
2020-09-02 11:27:12 +0200

    changed the location of .env file

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'backend/.env' });
+const path = require('path');
 const express = require('express');
 const app = express();
 const databaseFunctions = require('./db');
