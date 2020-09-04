b696178d2bc99fe5bb531774184d402089c8f5ad
Bartek Wilczyński <bartekw2213@gmail.com>
2020-09-04 12:34:48 +0200

    setting loading to true when fetching is started

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux';
 import {
   fetchRestaurantsByDefault,
   resetState,
+  setLoading,
 } from '../../redux/slices/restaurantsSlice';
 
 import './Homepage.css';
@@ -77,6 +78,7 @@ const Homepage = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     dispatch(resetState());
+    dispatch(setLoading(true));
     dispatch(fetchRestaurantsByDefault(formData));
     setRedirect(true);
   };
