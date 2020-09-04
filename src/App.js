5622906eeb02d8bb27a254e4cb405e7d27a6b0e6
Bartek Wilczyński <bartekw2213@gmail.com>
2020-09-04 12:33:51 +0200

    added the restaurants component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './components/Homepage/Homepage';
+import Restaurants from './components/Restaurants/Restaurants';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Homepage} />
+        <Route path='/restaurants' exact component={Restaurants} />
       </Switch>
     </div>
   );
